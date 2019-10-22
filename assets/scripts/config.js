1838ac0bbc4e2947f8ee21bd16523e0be199dfc1
Nancy4510 <nancy4510@gmail.com>
2019-10-22 16:32:30 -0400

    project2Client
    fixed a bug I had on the login page

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<https://peaceful-cove-13394.herokuapp.com/>',
+  production: 'https://peaceful-cove-13394.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
